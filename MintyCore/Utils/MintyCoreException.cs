2cf58184be39ee13cfc65a71039ca7bc1a864522
Alexander.Panick <alexander@familiepanick.de>
2021-07-11 22:28:25 +0200

    Added "MintyCoreException"

---
+++ MintyCore/Utils/MintyCoreException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintyCore.Utils
+{
+	class MintyCoreException : Exception
+	{
+		internal MintyCoreException()
+		{
+		}
+
+		internal MintyCoreException(string? message) : base(message)
+		{
+		}
+
+		internal MintyCoreException(string? message, Exception? innerException) : base(message, innerException)
+		{
+		}
+
+		internal MintyCoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}

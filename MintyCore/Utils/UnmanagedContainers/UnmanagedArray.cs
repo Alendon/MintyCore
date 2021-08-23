e2bdd5644ed87559a7bad99d32a3065829dc4e82
Alexander.Panick <alexander@familiepanick.de>
2021-08-23 18:28:57 +0200

    Added UnmanagedArray and dispose handler

---
+++ MintyCore/Utils/UnmanagedContainers/UnmanagedArray.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintyCore.Utils.UnmanagedContainers
+{
+	public unsafe struct UnmanagedArray<TItem> : IEnumerable<TItem> where TItem : unmanaged
+	{
+		public int Length { get; init; }
+		private readonly TItem* items;
+		private readonly UnmanagedDisposer<TItem>* _disposer;
+
+		public UnmanagedArray(int length, bool clearValues = true)
+		{
+			Length = length;
+			items = (TItem*)AllocationHandler.Malloc<TItem>(length);
+			_disposer = UnmanagedDisposer<TItem>.CreateDisposer(&DisposeItems, items);
+
+			if (clearValues)
+			{
+				for (int i = 0; i < length; i++)
+				{
+					items[i] = default;
+				}
+			}
+		}
+
+		public IEnumerator<TItem> GetEnumerator()
+		{
+			return new Enumerator(Length, items);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		/// <summary>
+		/// Increase the internal reference counter. For each reference the dispose method needs to be called once.
+		/// </summary>
+		public void IncreaseRefCount()
+		{
+			if (_disposer is not null)
+				_disposer->IncreaseRefCount();
+		}
+
+		public void DecreaseRefCount()
+		{
+			if (_disposer is not null)
+				_disposer->DecreaseRefCount();
+		}
+
+		private static void DisposeItems(TItem* items)
+		{
+			AllocationHandler.Free(new IntPtr(items));
+		}
+
+		public TItem this[int index]
+		{
+			get
+			{
+				if (index < 0 || index >= Length) throw new ArgumentOutOfRangeException();
+				return items[index];
+			}
+			set
+			{
+				if (index < 0 || index >= Length) throw new ArgumentOutOfRangeException();
+				items[index] = value;
+			}
+		}
+
+		private struct Enumerator : IEnumerator<TItem>
+		{
+			int _length;
+			TItem* _items;
+			int _index;
+
+			public Enumerator(int length, TItem* items)
+			{
+				_length = length;
+				_items = items;
+				_index = -1;
+			}
+
+			public TItem Current => _items[_index];
+
+			object IEnumerator.Current => Current;
+
+			public void Dispose()
+			{
+			}
+
+			public bool MoveNext()
+			{
+				_index++;
+				return _index < _length;
+			}
+
+			public void Reset()
+			{
+				throw new NotSupportedException();
+			}
+		}
+	}
+}
---
+++ MintyCore/Utils/UnmanagedContainers/UnmanagedDisposeHandler.cs
@@ -0,0 +1,67 @@
+﻿using MintyCore.ECS;
+using MintyCore.Identifications;
+using MintyCore.Utils;
+
+namespace MintyCore.Utils.UnmanagedContainers
+{
+	public unsafe struct UnmanagedDisposer<TRessource> where TRessource : unmanaged
+	{
+		private delegate* <TRessource*, void> _disposeFunction;
+		private readonly TRessource* _toDispose;
+		private byte _selfDispose;
+		private UnmanagedDisposer<TRessource>* self;
+
+		public int ReferenceCount { get; private set; }
+
+		public UnmanagedDisposer(delegate*<TRessource*, void> disposeFunction, TRessource* toDispose)
+		{
+			if (disposeFunction is null || toDispose is null)
+			{
+				Logger.WriteLog($"Tried to create an {nameof(UnmanagedDisposer<TRessource>)} with a null dispose function and/or a null dispose ressource", LogImportance.EXCEPTION, "Utils");
+			}
+
+			_disposeFunction = disposeFunction;
+			ReferenceCount = 1;
+			_toDispose = toDispose;
+			_selfDispose = 0;
+			self = null;
+		}
+
+		public static UnmanagedDisposer<TRessource>* CreateDisposer(delegate*<TRessource*, void> disposeFunction, TRessource* toDispose)
+		{
+			var ptr = (UnmanagedDisposer<TRessource>*)AllocationHandler.Malloc<UnmanagedDisposer<TRessource>>();
+			*ptr = new UnmanagedDisposer<TRessource>(disposeFunction, toDispose);
+			ptr->_selfDispose = 1;
+			ptr->self = ptr;
+			return ptr;
+		}
+
+		public void DecreaseRefCount()
+		{
+			ReferenceCount--;
+			CheckDispose();
+		}
+
+		public void IncreaseRefCount()
+		{
+			ReferenceCount++;
+		}
+
+		private void CheckDispose()
+		{
+			if(ReferenceCount <= 0 && _disposeFunction is not null && _toDispose is not null )
+			{
+				_disposeFunction(_toDispose);
+				if(_selfDispose != 0 && self is not null)
+				{
+					DisposeSelf(self);
+				}
+			}
+		}
+
+		private static void DisposeSelf(UnmanagedDisposer<TRessource>* instance)
+		{
+			AllocationHandler.Free(new System.IntPtr(instance));
+		}
+	}
+}

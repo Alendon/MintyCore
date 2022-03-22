ab6e2869cf8c8616993f5ecd444b11c70ae875b3
Alendon <Alexander@Familiepanick.de>
2022-03-22 11:05:44 +0100

    Removed auto generated parallel system

--- MintyCore/Systems/Common/ApplyTransformSystem.cs
+++ MintyCore/Systems/Common/ApplyTransformSystem.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
 using MintyCore.Components.Common;
 using MintyCore.ECS;
 using MintyCore.Identifications;
@@ -8,7 +10,6 @@ using MintyCore.Utils;
 namespace MintyCore.Systems.Common;
 
 [ExecuteInSystemGroup(typeof(FinalizationSystemGroup))]
-[ParallelSystem]
 internal partial class ApplyTransformSystem : ASystem
 {
     [ComponentQuery]
@@ -20,24 +21,16 @@ internal partial class ApplyTransformSystem : ASystem
     {
     }
 
-    protected override void Execute()
+    protected sealed override void Execute()
     {
-        foreach (var entity in _componentQuery)
-        {
-            var position = entity.GetPosition();
-            var rotation = entity.GetRotation();
-            var scale = entity.GetScale();
-            ref var transform = ref entity.GetTransform();
-
-            var value = Matrix4x4.CreateFromQuaternion(rotation.Value) *
-                        Matrix4x4.CreateTranslation(position.Value) *
-                        Matrix4x4.CreateScale(scale.Value);
-
-            transform.Dirty = 1;
-            transform.Value = value;
-        }
+        
     }
 
+    public override Task QueueSystem(IEnumerable<Task> dependencies)
+    {
+        return Task.WhenAll(dependencies).ContinueWith(_ => Parallel.ForEach(_componentQuery, Execute));
+    }
+    
     private void Execute(TestComponentQuery<Transform, (Position, Rotation, Scale)>.CurrentEntity entity)
     {
         var position = entity.GetPosition();

8e26cf0a296d94cc585ccbc9f697d190aff9a216
Alendon <Alexander.Panick@live.de>
2023-04-11 13:50:32 +0200

    Push to v0.2.1

--- MintyCore/MintyCoreMod.cs
+++ MintyCore/MintyCoreMod.cs
@@ -43,7 +43,7 @@ public sealed partial class MintyCoreMod : IMod
             {
                 "Alendon", "Erikiller"
             },
-            Version = new Version(0, 2),
+            Version = new Version(0, 2, 1),
             IsRootMod = true,
             Identifier = "minty_core",
             Description = "The base mod of the MintyCore engine",

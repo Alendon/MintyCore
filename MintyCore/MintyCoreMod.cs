6aa1cf351c675621df06f4d2236617c2e507d625
Alendon <Alexander@Familiepanick.de>
2023-04-13 13:12:24 +0200

    Increased Version number

--- MintyCore/MintyCoreMod.cs
+++ MintyCore/MintyCoreMod.cs
@@ -43,7 +43,7 @@ public sealed partial class MintyCoreMod : IMod
             {
                 "Alendon", "Erikiller"
             },
-            Version = new Version(0, 2, 1),
+            Version = new Version(0, 2, 2),
             IsRootMod = true,
             Identifier = "minty_core",
             Description = "The base mod of the MintyCore engine",

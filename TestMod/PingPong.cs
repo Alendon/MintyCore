bd43c722a9d363725bdf98d102ecc6b8d9eba90c
Alendon <Alexander.Panick@live.de>
2023-07-16 15:25:12 +0200

    Added simple ping pong test

---
+++ TestMod/PingPong.cs
@@ -0,0 +1,51 @@
+﻿using MintyCore;
+using MintyCore.Network;
+using MintyCore.Registries;
+using MintyCore.Utils;
+using Silk.NET.Input;
+
+namespace TestMod;
+
+[RegisterMessage("ping_pong")]
+public partial class PingPong : IMessage
+{
+    public bool IsServer { get; set; }
+    public bool ReceiveMultiThreaded => true;
+    public Identification MessageId => Identifications.MessageIDs.PingPong;
+    public DeliveryMethod DeliveryMethod => DeliveryMethod.Reliable;
+    public ushort Sender { get; set; }
+    public void Serialize(DataWriter writer)
+    {
+        writer.Put(DateTime.UtcNow.Ticks);
+    }
+
+    public bool Deserialize(DataReader reader)
+    {
+        if (!reader.TryGetLong(out var ticks)) return false;
+        var send = new DateTime(ticks);
+        var receive = DateTime.UtcNow;
+        var delay = receive - send;
+        
+        Logger.WriteLog($"Received ping from {PlayerHandler.GetPlayer(Sender).Name} with {delay.TotalMilliseconds}ms delay", LogImportance.Info, "TestMod");
+        return true;
+    }
+
+    public void Clear()
+    {
+        //Nothing to do here
+    }
+
+    [RegisterKeyAction("ping_pong")]
+    public static KeyActionInfo PingPongKey => new()
+    {
+        Action = (keyState, _) =>
+        {
+            if (keyState != KeyStatus.KeyDown) return;
+            
+            Logger.WriteLog("Sending ping", LogImportance.Info, "TestMod");
+            var message = new PingPong();
+            message.SendToServer();
+        },
+        Key = Key.P
+    };
+}
--- TestMod/Test.cs
+++ TestMod/Test.cs
@@ -1,32 +1,108 @@
 ﻿using MintyCore;
+using MintyCore.ECS;
 using MintyCore.Modding;
+using MintyCore.Network;
+using MintyCore.Render;
 using MintyCore.Utils;
+using MintyCore.Utils.Maths;
 
 namespace TestMod;
 
 public sealed partial class Test : IMod
 {
     public static Test Instance { get; private set; } = null!;
-    
+
     public void Dispose()
     {
         //Nothing to do here
     }
 
     public ushort ModId { get; set; }
+
     public void PreLoad()
     {
         Instance = this;
+
+        Engine.RunMainMenu = RunMainMenu;
+
+        Engine.RunHeadless = RunHeadless;
+    }
+
+    private void RunHeadless()
+    {
+        Logger.WriteLog("Welcome to the TestMod Headless!", LogImportance.Info, "TestMod");
         
-        Engine.RunMainMenu = () =>
-        {
-            Logger.WriteLog("Welcome to the TestMod MainMenu!", LogImportance.Info, "TestMod");   
-        };
+        Engine.SetGameType(GameType.Server);
+        Engine.LoadMods(ModManager.GetAvailableMods(true));
+        WorldHandler.CreateWorlds(GameType.Server);
+        Engine.CreateServer(Engine.HeadlessPort);
         
-        Engine.RunHeadless = () =>
+        GameLoop();
+    }
+
+    private void RunMainMenu()
+    {
+        Logger.WriteLog("Welcome to the TestMod MainMenu!", LogImportance.Info, "TestMod");
+        //TODO add a way to connect to a server
+        Logger.WriteLog("Currently it is only possible to create a local game", LogImportance.Info, "TestMod");
+        
+        Engine.SetGameType(GameType.Local);
+        PlayerHandler.LocalPlayerId = 1;
+        PlayerHandler.LocalPlayerName = "Local";
+        
+        Engine.LoadMods(ModManager.GetAvailableMods(true));
+        
+        WorldHandler.CreateWorlds(GameType.Server);
+        
+        Engine.CreateServer(Constants.DefaultPort);
+        Engine.ConnectToServer("localhost", Constants.DefaultPort);
+        
+        GameLoop();
+    }
+
+    private static void GameLoop()
+    {
+        //If this is a client game (client or local) wait until the player is connected
+        while (MathHelper.IsBitSet((int)Engine.GameType, (int)GameType.Client) &&
+               PlayerHandler.LocalPlayerGameId == Constants.InvalidId)
+            NetworkHandler.Update();
+
+        Engine.DeltaTime = 0;
+        Engine.Timer.Reset();
+        while (!Engine.Stop)
         {
-            Logger.WriteLog("Welcome to the TestMod Headless!", LogImportance.Info, "TestMod");   
-        };
+            Engine.Timer.Tick();
+            Engine.Window?.DoEvents();
+
+            var simulationEnable = Engine.Timer.GameUpdate(out var deltaTime);
+            Engine.DeltaTime = deltaTime;
+            
+            var drawingEnable = false;
+            if(MathHelper.IsBitSet((int)Engine.GameType, (int)GameType.Client))
+            {
+                drawingEnable = Engine.Timer.RenderUpdate(out var renderDeltaTime) && VulkanEngine.PrepareDraw();
+                Engine.RenderDeltaTime = renderDeltaTime;
+            }
+
+            WorldHandler.UpdateWorlds(GameType.Local, simulationEnable, drawingEnable);
+
+
+            if (drawingEnable)
+            {
+                VulkanEngine.EndDraw();
+            }
+
+            WorldHandler.SendEntityUpdates();
+
+            NetworkHandler.Update();
+
+
+            Logger.AppendLogToFile();
+            if (simulationEnable)
+                Engine.Tick++;
+        }
+        
+        Engine.CleanupGame();
     }
 
     public void Load()

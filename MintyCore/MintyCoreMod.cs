9b48018d84787442279df9e052ebc2504034ff12
Alendon <Alexander.Panick@live.de>
2024-10-09 10:45:34 +0200

    Updating Packages

--- MintyCore/MintyCoreMod.cs
+++ MintyCore/MintyCoreMod.cs
@@ -24,7 +24,7 @@ public sealed class MintyCoreMod(IVulkanEngine vulkanEngine) : IMod
             {
                 "Alendon", "Erikiller"
             },
-            Version = new Version(0, 7, 3),
+            Version = new Version(0, 7, 5),
             IsRootMod = true,
             Identifier = "minty_core",
             Description = "The base mod of the MintyCore engine",
